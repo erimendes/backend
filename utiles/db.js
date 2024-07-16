c2bf9ed5db1e0da2738d5d36647d94b4572e2614
erimendes <eri.mendes32@gmial.com>
2024-07-16 03:47:28 +0100

    Alteração do arquivo db.js

--- utiles/db.js
+++ utiles/db.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose");
 
 module.exports.dbConnect = async () => {
   try {
-    await mongoose.connect(process.env.DB_URL, { useNewURLParser: true });
+    await mongoose.connect(process.env.DB_URL);
     console.log("Database connected..");
   } catch (error) {
     console.log("Mongoose connection error:", error.message);
